5e3832f34c94762df8b343df41eeb323adccc72e
jfisher721 <113958054+jfisher721@users.noreply.github.com>
2022-11-16 15:40:09 -0800

    Added all tables
    Tables added: characters, winners, character_stats, teams, team_members, items, inventory and equipped

--- week7/answers.sql
+++ week7/answers.sql
@@ -1,10 +1,105 @@
 -- Create your tables, views, functions and procedures here!
+DROP SCHEMA destruction;
 CREATE SCHEMA destruction;
 USE destruction;
 
+-- Creating tables
 CREATE TABLE players (
-  player_id INT UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  first_name VARCHAR(30) NOT NULL,
-  last_name VARCHAR(30) NOT NULL,
-  email VARCHAR(50) NOT NULL
+	player_id INT UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
+	first_name VARCHAR(30) NOT NULL,
+	last_name VARCHAR(30) NOT NULL,
+	email VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE characters (
+	character_id INT UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    player_id INT UNSIGNED NOT NULL,
+    name VARCHAR(30) NOT NULL,
+    level TINYINT UNSIGNED NOT NULL,
+    CONSTRAINT characters_fk_players
+		FOREIGN KEY (player_id)
+        REFERENCES players (player_id)
+        ON UPDATE CASCADE
+        ON DELETE CASCADE
+);
+
+CREATE TABLE winners (
+	character_id INT UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    name VARCHAR(30) NOT NULL,
+    CONSTRAINT winners_fk_characters
+		FOREIGN KEY (character_id)
+        REFERENCES characters (character_id)
+        ON UPDATE CASCADE
+        ON DELETE CASCADE
+);
+
+CREATE TABLE character_stats (
+	character_id INT UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    health INT SIGNED NOT NULL,
+    armor INT UNSIGNED NOT NULL,
+    CONSTRAINT character_stats_fk_characters
+		FOREIGN KEY (character_id)
+        REFERENCES characters (character_id)
+        ON UPDATE CASCADE
+        ON DELETE CASCADE
+);
+
+CREATE TABLE teams (
+	team_id INT UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE team_members (
+	team_member_id INT UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    team_id INT UNSIGNED NOT NULL,
+    character_id INT UNSIGNED NOT NULL,
+    CONSTRAINT team_members_fk_team
+		FOREIGN KEY (team_id)
+        REFERENCES teams (team_id)
+        ON UPDATE CASCADE
+        ON DELETE CASCADE,
+	CONSTRAINT team_members_fk_characters
+		FOREIGN KEY (character_id)
+        REFERENCES characters (character_id)
+        ON UPDATE CASCADE
+        ON DELETE CASCADE  
+);
+    
+CREATE TABLE items (
+	item_id INT UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    name VARCHAR(30) NOT NULL,
+    armor INT UNSIGNED NOT NULL,
+    damage INT UNSIGNED NOT NULL
+);
+
+CREATE TABLE inventory (
+	inventory_id INT UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    character_id INT UNSIGNED NOT NULL,
+    item_id INT UNSIGNED NOT NULL,
+    CONSTRAINT inventory_fk_characters
+		FOREIGN KEY (character_id)
+        REFERENCES characters (character_id)
+        ON UPDATE CASCADE
+        ON DELETE CASCADE,
+	CONSTRAINT inventory_fk_items
+		FOREIGN KEY (item_id)
+        REFERENCES items (item_id)
+		ON UPDATE CASCADE
+        ON DELETE CASCADE
+);
+
+CREATE TABLE equipped (
+	equipped_id INT UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    character_id INT UNSIGNED NOT NULL,
+    item_id INT UNSIGNED NOT NULL,
+    CONSTRAINT equipped_fk_characters
+		FOREIGN KEY (character_id)
+        REFERENCES characters (character_id)
+        ON UPDATE CASCADE
+        ON DELETE CASCADE,
+	CONSTRAINT equipped_fk_items
+		FOREIGN KEY (item_id)
+        REFERENCES items (item_id)
+        ON UPDATE CASCADE
+        ON DELETE CASCADE
 );
