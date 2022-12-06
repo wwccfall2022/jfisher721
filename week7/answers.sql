b7fd46066b18aa57f9b8fdb829b6b6d6daef6149
jfisher721 <113958054+jfisher721@users.noreply.github.com>
2022-12-06 15:17:20 -0800

    Edited attack procedure

--- week7/answers.sql
+++ week7/answers.sql
@@ -175,7 +175,7 @@ BEGIN
 			ON e.item_id = it.item_id
 		WHERE e.equipped_id = weapon_used;
         
-	SET result = character_armor - character_damage;
+	SET result = character_damage - character_armor;
     
     SELECT cs.health INTO character_health
 		FROM character_stats cs
